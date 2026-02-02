5b2ac913c3ddb8dcb8f0604a6cb2e6009322bcb1
2klxst <123006919+2k-lxst@users.noreply.github.com>
2026-02-02 15:29:09 +0100

    Enhance UI testing setup and integrate BuildUI for dynamic parameter management

--- main_window_test.py
+++ main_window_test.py
@@ -1,16 +1,23 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QDoubleSpinBox, QPlainTextEdit
 
 # Import compiled UI class and Resources
 from ui.main_window_ui import Ui_MainWindow
 
 from modelViewer import ModelViewer
+from build_ui import BuildUI
 
 class TestWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.build_ui = BuildUI()
+
+        self.build_ui.populate_toolbox(self.ui.parametersToolBox) # type: ignore
+
+        self.setup_testing_connections()
 
         # Set a title for the test UI
         self.setWindowTitle("BoxCAD - UI Test Environment")
@@ -20,6 +27,17 @@ class TestWindow(QMainWindow):
         layout = QVBoxLayout(container)  # Add a layout to the placeholder # type: ignore
         self.viewer = ModelViewer()       # Create your viewer widget
         layout.addWidget(self.viewer)     # Put the viewer in the layout
+
+    def setup_testing_connections(self):
+        # We look for the widgets we stored in the factory's dictionary
+        for name, widget in self.build_ui.widgets.items():
+            if isinstance(widget, QDoubleSpinBox):
+                widget.valueChanged.connect(lambda val, n=name: self.debug_print(n, val))
+            elif isinstance(widget, QPlainTextEdit):
+                widget.textChanged.connect(lambda n=name: self.debug_print(n, "Text Updated"))
+
+    def debug_print(self, name, value):
+        print(f"TEST: Parameter '{name}' changed to: {value}")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- ui/main_window_ui.py
+++ ui/main_window_ui.py
@@ -16,10 +16,9 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QDockWidget, QDoubleSpinBox, QFormLayout,
-    QFrame, QGridLayout, QLabel, QLayout,
-    QMainWindow, QMenu, QMenuBar, QSizePolicy,
-    QStatusBar, QToolBox, QWidget)
+from PySide6.QtWidgets import (QApplication, QDockWidget, QFormLayout, QFrame,
+    QGridLayout, QMainWindow, QMenu, QMenuBar,
+    QSizePolicy, QStatusBar, QToolBox, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -86,86 +85,18 @@ class Ui_MainWindow(object):
         self.formLayout.setFieldGrowthPolicy(QFormLayout.AllNonFixedFieldsGrow)
         self.formLayout.setVerticalSpacing(10)
         self.formLayout.setContentsMargins(12, 10, 12, 12)
-        self.toolBox = QToolBox(self.dockWidgetContents)
-        self.toolBox.setObjectName(u"toolBox")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.MinimumExpanding)
-        sizePolicy2.setHorizontalStretch(0)
-        sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(self.toolBox.sizePolicy().hasHeightForWidth())
-        self.toolBox.setSizePolicy(sizePolicy2)
-        self.toolBox.setMinimumSize(QSize(0, 170))
-        self.page_3 = QWidget()
-        self.page_3.setObjectName(u"page_3")
-        self.page_3.setGeometry(QRect(0, 0, 176, 142))
-        self.formLayout_2 = QFormLayout(self.page_3)
-        self.formLayout_2.setObjectName(u"formLayout_2")
-        self.formLayout_2.setSizeConstraint(QLayout.SetMinAndMaxSize)
-        self.lengthLabel = QLabel(self.page_3)
-        self.lengthLabel.setObjectName(u"lengthLabel")
-        font = QFont()
-        font.setFamilies([u"Inter 18pt"])
-        font.setPointSize(8)
-        self.lengthLabel.setFont(font)
+        self.parametersToolBox = QToolBox(self.dockWidgetContents)
+        self.parametersToolBox.setObjectName(u"parametersToolBox")
+        self.page = QWidget()
+        self.page.setObjectName(u"page")
+        self.page.setGeometry(QRect(0, 0, 170, 69))
+        self.parametersToolBox.addItem(self.page, u"Page 1")
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.page_2.setGeometry(QRect(0, 0, 170, 69))
+        self.parametersToolBox.addItem(self.page_2, u"Page 2")
 
-        self.formLayout_2.setWidget(0, QFormLayout.ItemRole.LabelRole, self.lengthLabel)
-
-        self.lengthInput = QDoubleSpinBox(self.page_3)
-        self.lengthInput.setObjectName(u"lengthInput")
-        self.lengthInput.setMinimumSize(QSize(0, 0))
-
-        self.formLayout_2.setWidget(0, QFormLayout.ItemRole.FieldRole, self.lengthInput)
-
-        self.widthLabel = QLabel(self.page_3)
-        self.widthLabel.setObjectName(u"widthLabel")
-        self.widthLabel.setFont(font)
-
-        self.formLayout_2.setWidget(1, QFormLayout.ItemRole.LabelRole, self.widthLabel)
-
-        self.widthInput = QDoubleSpinBox(self.page_3)
-        self.widthInput.setObjectName(u"widthInput")
-
-        self.formLayout_2.setWidget(1, QFormLayout.ItemRole.FieldRole, self.widthInput)
-
-        self.heightLabel = QLabel(self.page_3)
-        self.heightLabel.setObjectName(u"heightLabel")
-        self.heightLabel.setFont(font)
-
-        self.formLayout_2.setWidget(2, QFormLayout.ItemRole.LabelRole, self.heightLabel)
-
-        self.heightInput = QDoubleSpinBox(self.page_3)
-        self.heightInput.setObjectName(u"heightInput")
-
-        self.formLayout_2.setWidget(2, QFormLayout.ItemRole.FieldRole, self.heightInput)
-
-        self.heightLabel_2 = QLabel(self.page_3)
-        self.heightLabel_2.setObjectName(u"heightLabel_2")
-        self.heightLabel_2.setFont(font)
-
-        self.formLayout_2.setWidget(3, QFormLayout.ItemRole.LabelRole, self.heightLabel_2)
-
-        self.heightInput_2 = QDoubleSpinBox(self.page_3)
-        self.heightInput_2.setObjectName(u"heightInput_2")
-
-        self.formLayout_2.setWidget(3, QFormLayout.ItemRole.FieldRole, self.heightInput_2)
-
-        self.heightInput_3 = QDoubleSpinBox(self.page_3)
-        self.heightInput_3.setObjectName(u"heightInput_3")
-
-        self.formLayout_2.setWidget(4, QFormLayout.ItemRole.FieldRole, self.heightInput_3)
-
-        self.heightLabel_3 = QLabel(self.page_3)
-        self.heightLabel_3.setObjectName(u"heightLabel_3")
-        self.heightLabel_3.setFont(font)
-
-        self.formLayout_2.setWidget(4, QFormLayout.ItemRole.LabelRole, self.heightLabel_3)
-
-        self.toolBox.addItem(self.page_3, u"Dimensions")
-        self.page_4 = QWidget()
-        self.page_4.setObjectName(u"page_4")
-        self.page_4.setGeometry(QRect(0, 0, 176, 116))
-        self.toolBox.addItem(self.page_4, u"Internal Hardware")
-
-        self.formLayout.setWidget(0, QFormLayout.ItemRole.SpanningRole, self.toolBox)
+        self.formLayout.setWidget(0, QFormLayout.ItemRole.FieldRole, self.parametersToolBox)
 
         self.parametersDock.setWidget(self.dockWidgetContents)
         MainWindow.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.parametersDock)
@@ -177,7 +108,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.toolBox.setCurrentIndex(0)
+        self.parametersToolBox.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -191,12 +122,7 @@ class Ui_MainWindow(object):
         self.menuExit.setTitle(QCoreApplication.translate("MainWindow", u"Exit", None))
         self.outputDock.setWindowTitle(QCoreApplication.translate("MainWindow", u"Hardware Library", None))
         self.parametersDock.setWindowTitle(QCoreApplication.translate("MainWindow", u"Parameters", None))
-        self.lengthLabel.setText(QCoreApplication.translate("MainWindow", u"Length (X)", None))
-        self.widthLabel.setText(QCoreApplication.translate("MainWindow", u"Width (Y)", None))
-        self.heightLabel.setText(QCoreApplication.translate("MainWindow", u"Height (Z)", None))
-        self.heightLabel_2.setText(QCoreApplication.translate("MainWindow", u"Wall Thickness", None))
-        self.heightLabel_3.setText(QCoreApplication.translate("MainWindow", u"Corner Radius", None))
-        self.toolBox.setItemText(self.toolBox.indexOf(self.page_3), QCoreApplication.translate("MainWindow", u"Dimensions", None))
-        self.toolBox.setItemText(self.toolBox.indexOf(self.page_4), QCoreApplication.translate("MainWindow", u"Internal Hardware", None))
+        self.parametersToolBox.setItemText(self.parametersToolBox.indexOf(self.page), QCoreApplication.translate("MainWindow", u"Page 1", None))
+        self.parametersToolBox.setItemText(self.parametersToolBox.indexOf(self.page_2), QCoreApplication.translate("MainWindow", u"Page 2", None))
     # retranslateUi
 
